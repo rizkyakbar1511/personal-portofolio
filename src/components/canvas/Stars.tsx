585066c4cb40bbc92c8283f8241b31516b0f0a1e
Rizki Akbar <rizkyakbar1511@gmail.com>
2023-10-03 21:50:39 +0700

    fix: typed array points positions & computed radius

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -4,18 +4,18 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 
 import * as random from "maath/random/dist/maath-random.cjs";
 
-const Stars = (props) => {
-  const ref = useRef(null);
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+const Stars = () => {
+  const ref = useRef<THREE.Points>(null!);
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5001), { radius: 1.2 }));
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 10;
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={new Float32Array(sphere)} stride={3} frustumCulled>
         <PointMaterial transparent color="#f272c8" size={0.002} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
